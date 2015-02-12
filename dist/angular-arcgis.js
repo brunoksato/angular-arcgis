3e8af1273bf4e7aecd15edeebf787feb776af244
brunoksato <bruno.sato@live.com>
2015-02-12 19:41:31 -0200

    Crição do projeto

---
+++ dist/angular-arcgis.js
@@ -0,0 +1,110 @@
+'use strict';
+
+/**
+* esri.map Module
+*
+* Description
+*/
+angular.module('esri.map', [])
+    .directive('Map', Map)
+    .factory('MapService', MapService)
+    .controller('MapCtrl', MapCtrl);
+
+var mapObject = function(){
+
+    this.map = undefined;
+
+}
+
+function MapService(){
+
+    return mapObject;
+
+}
+
+require(['esri/map'], function (Map) {
+
+    mapObject.map = Map;
+    mapObject.scope.recreateMap();
+
+});
+
+/**
+ * [Map description]
+ * @ngInject
+ */
+function Map(){
+    return{
+
+        restrict: 'E',
+        link: function($scope, $element, $attr, $ctrl){
+
+            $ctrl.init($element);
+
+        }
+
+    }
+}
+
+function MapCtrl($rootScope, $scope, $attrs, MapService){
+
+    var mapDiv;
+    $scope.$element;
+    $scope.mapService = MapService;
+    $scope.mapService.scope = $scope;
+
+    this.init = function (element) {
+
+        if (!$attrs.id) { throw new Error('\'id\' is required for a map.'); }
+        $scope.$element = element;
+
+        if(!$scope.mapService.map){
+            return;
+        }
+
+        $scope.recreateMap();
+    };
+
+    $scope.recreateMap = function(){
+
+        createDiv();
+        createMap();
+
+    };
+
+    function createDiv(){
+
+        if(mapDiv){
+            return;
+        }
+        mapDiv = document.createElement('div');
+        mapDiv.setAttribute('id', $attrs.id);
+        $scope.$element.removeAttr('id');
+        $scope.$element.append(mapDiv);
+
+    }
+
+    function createMap(){
+
+        if(!$scope.mapService.map){
+            return;
+        }
+        if(!mapDiv){
+            return;
+        }
+
+        var options = {
+            center: $attrs.center ? JSON.parse($attrs.center) : [-56.049, 38.485],
+            zoom: $attrs.zoom ? parseInt($attrs.zoom) : 10,
+            basemap: $attrs.basemap ? $attrs.basemap : 'streets'
+        };
+
+        $scope.map = new $scope.mapService.map($attrs.id, options);
+
+        $scope.map.on('load', function () { $rootScope.$broadcast('map-load'); });
+        $scope.map.on('click', function (e) { $rootScope.$broadcast('map-click', e); });
+
+    }
+
+}
+MapCtrl.$inject = ["$rootScope", "$scope", "$attrs", "MapService"];
---
+++ gulpfile.js
@@ -0,0 +1,82 @@
+/**
+ * Created by Bruno Sato
+ * npm install gulp gulp-util gulp-rename gulp-minify-css gulp-jshint gulp-concat gulp-clean gulp-notify gulp-uglify gulp-ng-annotate gulp-autoprefixer --save-dev
+ */
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+
+var $ = require('gulp-load-plugins')({
+  pattern: ['gulp-*']
+});
+
+
+gulp.task('serve', ['browser-sync'], function(){
+
+    gulp.watch("example/*.js", ['js', browserSync.reload]);
+    gulp.watch("example/*.html", ['html', browserSync.reload]);
+
+});
+
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: "./example"
+        }
+    });
+});
+
+
+gulp.task('build', ['scripts','styles','lint','clean'], function() {
+
+  gulp.src("./src/notify.ext")
+    .pipe($.notify("Build Complete!"));
+
+});
+
+
+gulp.task('lint', function() {
+    gulp.src('src/*.js')
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('default'));
+});
+
+
+gulp.task('styles', function() {
+    gulp.src(['src/styles/*.css'])
+        .pipe($.autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe($.rename("angular-arcgis.css"))
+        .pipe(gulp.dest('dist/'));
+
+    gulp.src(['src/styles/*.css'])
+        .pipe($.autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe($.rename("angular-arcgis.min.css"))
+        .pipe($.minifyCss())
+        .pipe(gulp.dest('dist/'));
+});
+
+
+gulp.task('scripts', function() {
+    gulp.src('src/**/*.js')
+        .pipe($.ngAnnotate())
+        .pipe($.concat('angular-arcgis.js'))
+        .pipe(gulp.dest('example/'))
+        .pipe(gulp.dest('dist/'));
+
+    gulp.src('src/**/*.js')
+        .pipe($.ngAnnotate())
+        .pipe($.uglify())
+        .pipe($.rename('angular-arcgis.min.js'))
+        .pipe(gulp.dest('dist/'));
+
+    gulp.src('bower_components/angular/angular.min.js')
+        .pipe(gulp.dest('example/'));
+
+});
+
+
+gulp.task('clean', function() {
+    return gulp.src(['dist/*', 'example/*.js'], {read: false})
+        .pipe($.clean());
+});
