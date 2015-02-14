08154c59ac5f55c5c3cd3a9a583a0ebaf304c858
brunoksato <bruno.sato@live.com>
2015-02-14 16:02:51 -0200

    create test module, and add test task gulp

--- test/esri.service.spec.js
+++ test/esri.service.spec.js
@@ -2,21 +2,10 @@ describe('Test Module Esri Service', function(){
 
 	beforeEach(angular.mock.module('esri.service'));
 
-	beforeEach(inject( function( EsriBaseMaps ){
-
-		baseMaps = EsriBaseMaps;
-
-	}));
-
-	it('have modulo service', function(  ){
+	it('exist module esri.service', function(  ){
 
 		expect('esri.service').toBeDefined();
 
 	});
 
-	it('Test EsriBaseMaps exist', function(  ){
-
-		expect(baseMaps).toBeDefined();
-
-	});
 });
